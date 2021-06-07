d5887091dc8702bf8d570e0bbf834e53778a9079
AmirGiz <amirxan.gizatullin@bk.ru>
2021-06-07 13:10:59 +0300

    Фикс проблем с моделью наивного баеса

--- manage.py
+++ manage.py
@@ -3,6 +3,25 @@
 import os
 import sys
 
+import nltk
+from nltk.stem import SnowballStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+import string
+
+
+def tokenize_sentence(sentence: str):
+    snowball = SnowballStemmer(language="english")
+    stop_words = stopwords.words("english")
+
+    sanitized_sentence = sentence.replace("'", "").replace('"', "")
+    tokens = word_tokenize(sanitized_sentence, language="english")
+    tokens = [i for i in tokens if i not in string.punctuation]
+    tokens = [i for i in tokens if i not in stop_words]
+    tokens = [snowball.stem(i) for i in tokens]
+    return tokens
+
 
 def main():
     """Run administrative tasks."""
@@ -20,3 +39,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    tokenize_sentence()
